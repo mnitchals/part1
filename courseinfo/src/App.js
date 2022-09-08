5d471d3988d580a9c91093fcb5e00164f68ffc64
Matthew Nitchals <matthew.nitchals@gmail.com>
2022-09-07 21:17:26 -0500

    refactored components

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -3,10 +3,20 @@ const Header = (props) => {
     <h1>{props.course}</h1>
   )
 }
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+}
+
 
 const Content = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <div>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
+    </div>
   )
 }
 
@@ -28,9 +38,7 @@ const App = () => {
   return (
     <div>
       <Header  course={course}/>
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
       <Total part1={exercises1} part2={exercises2} part3={exercises3} />
     </div>
   )
